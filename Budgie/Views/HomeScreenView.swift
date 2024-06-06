cc95d2ef4c7e10426abf785a4aaedeafce383d5f
Veda Cunniffe <vcunniffe@lcs.on.ca>
2024-06-06 15:17:33 -0400

    Made a better (but still incomplete) home screen interface.

--- Budgie/Views/HomeScreenView.swift
+++ Budgie/Views/HomeScreenView.swift
@@ -14,11 +14,40 @@ struct HomeScreenView: View {
                 LinearGradient(colors: [Color.homeGradientTop, Color.homeGradientBottom], startPoint: .topTrailing, endPoint: .bottomLeading)
                     .ignoresSafeArea()
                 VStack {
+                    HStack {
+                        Text("Welcome")
+                            .font(
+                                .custom(
+                                    "Avenir-black",
+                                    size: 30,
+                                    relativeTo: .title
+                                )
+                        )
+                            .padding(EdgeInsets(top: 30, leading: 20, bottom: 5, trailing: 0))
+                        Spacer()
+                    }
+                    HStack {
+                        Text("Add a new budget or look at your previous budgets")
+                            .font(
+                                .custom(
+                                    "Helvetica",
+                                    size: 20
+                                )
+                        )
+                            .padding(EdgeInsets(top: 0, leading: 20, bottom: 60, trailing: 0))
+                        Spacer()
+                    }
                     NavigationLink {
                         FinalBudgetView()
                     } label: {
                         Text("Budget name placeholder")
                     }
+                    NavigationLink {
+                        QuestionPageView(page: question1)
+                    } label: {
+                        Text("+ Add new")
+                    }
+                    Spacer()
                 }
             }
         }
