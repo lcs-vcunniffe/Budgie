1acf30c97c41530c693ca7fb337cebe391c38d4b
Veda Cunniffe <vcunniffe@lcs.on.ca>
2024-06-10 15:44:28 -0400

    Reformatted the home screen to make it look better.

--- Budgie/Views/HomeScreenView.swift
+++ Budgie/Views/HomeScreenView.swift
@@ -22,7 +22,7 @@ struct HomeScreenView: View {
                                     size: 30,
                                     relativeTo: .title
                                 )
-                        )
+                            )
                             .padding(EdgeInsets(top: 30, leading: 20, bottom: 5, trailing: 0))
                         Spacer()
                     }
@@ -33,19 +33,33 @@ struct HomeScreenView: View {
                                     "Helvetica",
                                     size: 20
                                 )
-                        )
+                            )
                             .padding(EdgeInsets(top: 0, leading: 20, bottom: 60, trailing: 0))
                         Spacer()
                     }
-                    NavigationLink {
-                        FinalBudgetView()
-                    } label: {
-                        Text("Budget name placeholder")
+                    List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+                        NavigationLink {
+                            FinalBudgetView()
+                        } label: {
+                            Text("Budget name placeholder")
+                        }
+                        .listRowBackground(Color.clear)
                     }
+                    .listStyle(.plain)
                     NavigationLink {
                         AskQuestionView()
                     } label: {
                         Text("+ Add new")
+                            .font(
+                                .custom(
+                                    "Georgia",
+                                    size: 18
+                                )
+                            )
+                            .padding(EdgeInsets(top: 7, leading: 15, bottom: 7, trailing: 15))
+                            .foregroundStyle(Color.white)
+                            .background(Color.black)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
                     }
                     Spacer()
                 }
