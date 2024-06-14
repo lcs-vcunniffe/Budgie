b12c9368bcf9810a2e1ceb10adc48b50cc7d4264
Veda Cunniffe <vcunniffe@lcs.on.ca>
2024-06-14 09:30:11 -0400

    Added a new variable and file to make the button on the final budget view redirect to the chart screen, similar to the one on the start screen.

--- Budgie/Views/FinalBudgetView.swift
+++ Budgie/Views/FinalBudgetView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct FinalBudgetView: View {
     //MARK: Stored properties
     let finalBudget: FinalBudget
+    @Binding var moveToChart: Bool
     
     //MARK: Computed properties
     var body: some View {
@@ -57,7 +58,7 @@ struct FinalBudgetView: View {
                 Rectangle()
                     .fill(Color.clear)
                     .frame(height: 50)
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Button(action: {moveToChart = true}) {
                     Text("Continue")
                         .font(
                             .custom(
@@ -76,5 +77,5 @@ struct FinalBudgetView: View {
 }
 
 #Preview {
-    FinalBudgetView(finalBudget: budgetA)
+    FinalBudgetView(finalBudget: budgetA, moveToChart: Binding.constant(false))
 }
---
+++ Budgie/Views/FindingBudgetView.swift
@@ -0,0 +1,27 @@
+//
+//  FindingBudgetView.swift
+//  Budgie
+//
+//  Created by Veda Niav Cunniffe on 2024-06-14.
+//
+
+import SwiftUI
+
+struct FindingBudgetView: View {
+    
+    //MARK: Stored properties
+    @State private var hasMovedToChart = false
+    
+    //MARK: Computed properties
+    var body: some View {
+        if hasMovedToChart == false {
+            FinalBudgetView(finalBudget: <#T##FinalBudget#>, moveToChart: $hasMovedToChart)
+        } else {
+            ChartScreenView()
+        }
+    }
+}
+
+#Preview {
+    FindingBudgetView()
+}
