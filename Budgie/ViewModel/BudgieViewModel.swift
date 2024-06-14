3b9aa92b777acbfcd0f62f787c810896e5e914a0
Veda Cunniffe <vcunniffe@lcs.on.ca>
2024-06-13 21:53:32 -0400

    Referencing the to-do list app, made a function to get the made budgets for the home screen in the view model.

--- Budgie/ViewModel/BudgieViewModel.swift
+++ Budgie/ViewModel/BudgieViewModel.swift
@@ -10,5 +10,41 @@ import Foundation
 @Observable
 class BudgieViewModel {
     //MARK: Stored properties
+    //The list of budgets on the home screen
+    var budget: [FinalBudget]
+    //Track when the budgets are being fetched
+    var fetchingBudgets: Bool = false
     
+    //MARK: Initializer(s)
+    init(budget: [FinalBudget] = []) {
+        self.budget = budget
+        Task {
+            try await getTodos()
+        }
+    }
+    
+    //MARK: Functions
+    func getTodos() async throws {
+            
+        // Say that the app is getting the budgets
+        fetchingBudgets = true
+        
+        do {
+            let results: [FinalBudget] = try await database
+                .from("budget")
+                .select()
+                .order("id", ascending: true)
+                .execute()
+                .value
+            
+            self.budget = results
+            
+            // Show the budgets now that they have been fetched
+            fetchingBudgets = false
+            
+        } catch {
+            debugPrint(error)
+        }
+        
+    }
 }
